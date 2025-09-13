f4399a5ef7c5318b23b57778e201c486b2d52da0
FaizanShakeel <faizan.shakeel786011@gmail.com>
2025-09-13 10:21:20 +0500

    url update

--- src/config/data.js
+++ src/config/data.js
@@ -1,4 +1,4 @@
 // export const Frontend_url = "http://localhost:3000"
 // export const Backend_url = "http://localhost:8000"
-export const Frontend_url = "https://rep-content-main-cbh6fxd9cydvdvc8.canadacentral-01.azurewebsites.net"
+export const Frontend_url = "https://app.repcontent.com"
 export const Backend_url = "https://rep-content-backend-ghf4czghcfbpcxdh.canadacentral-01.azurewebsites.net"
